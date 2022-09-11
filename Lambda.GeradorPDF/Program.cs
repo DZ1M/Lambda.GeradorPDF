916e878e3cf039986e43987e763bc86998da30f6
Richard-Trage <richard.trage@sinerlog.global>
2022-09-11 18:41:28 -0300

    Adicionar arquivos de projeto.

---
+++ Lambda.GeradorPDF/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton<IPdfGeneratorHelper, PdfGeneratorHelper>();
+
+// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
+// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
+var app = builder.Build();
+
+
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapGet("/", () => "Welcome to running ASP.NET Core Minimal API on AWS Lambda");
+
+app.Run();
